17bcdbf4082df97c00120338e5ac5f7543c125c0
baicie <2055572879@qq.com>
2024-06-19 23:09:18 +0800

    19

---
+++ src/344.反转字符串.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=cpp
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i = 0; i < s.size() / 2; i++) {
+            swap(s[i], s[s.size() - i - 1]);
+        }
+    }
+};
+// @lc code=end
+
