c5c6594bc65164cc571b5d7fc788fe44b565278c
刘志伟 <2055572879@qq.com>
2024-07-12 11:15:26 +0800

    feat group

--- scripts/cpp-to-md.ts
+++ scripts/cpp-to-md.ts
@@ -3,6 +3,14 @@ import fs from "node:fs";
 import path from "node:path";
 import fse from "fs-extra";
 
+interface SliderItem {
+  text: string;
+  link?: string;
+  index: number;
+  collapsed?: boolean;
+  items?: Partial<SliderItem>[];
+}
+
 const rootPath = path.resolve(__dirname, "..");
 const vitePressDocPath = path.resolve(rootPath, "docs", "algorithm");
 const sliderPath = path.resolve(
@@ -14,11 +22,40 @@ const sliderPath = path.resolve(
 );
 const algorithmMDPath = path.resolve(rootPath, "docs", "algorithm.md");
 
+let sidebar = `
+export default 
+`;
+const sidebars: SliderItem[] = [];
+
 const files = globSync("../src/*.cpp", {
   cwd: __dirname,
   onlyFiles: true,
   absolute: true,
 });
+
+function sortSlider() {
+  const res = sidebars.sort((a, b) => a.index - b.index);
+  const maxGroup = Math.ceil(res[res.length - 1].index / 100);
+  const tree = Array(maxGroup)
+    .fill(null)
+    .map((_, index) => {
+      const min = index * 100 - 1;
+      const max = (index + 1) * 100;
+      const treeItem: Partial<SliderItem> = {
+        text: `Group ${min}-${max}`,
+        collapsed: true,
+        items: res
+          .filter((item) => item.index >= min && item.index <= max)
+          .map((item) => ({
+            text: item.text,
+            link: item.link,
+          })),
+      };
+      return treeItem;
+    });
+  sidebar += JSON.stringify(tree);
+  fs.writeFileSync(sliderPath, sidebar, { encoding: "utf-8" });
+}
 
 async function main() {
   // empty
@@ -28,24 +65,23 @@ async function main() {
     algorithmMDPath,
     path.resolve(vitePressDocPath, "algorithm.md")
   );
-  let sidebar = `
-    export default [
-  `;
+
   for (const file of files) {
     const fileName = path.basename(file, path.extname(file));
+    const tagetPath = `${path.resolve(vitePressDocPath, fileName)}.md`;
     const content = fs.readFileSync(file, { encoding: "utf-8" });
     const newContent = `\`\`\` cpp
         ${content}
     \`\`\`
     `;
-    const tagetPath = `${path.resolve(vitePressDocPath, fileName)}.md`;
     fs.writeFileSync(tagetPath, newContent, { encoding: "utf-8" });
-    sidebar += `{text: "${path.basename(
-      fileName
-    )}", link: "${encodeURIComponent(fileName)}"},`;
+    sidebars.push({
+      text: path.basename(fileName),
+      link: `/${encodeURIComponent(fileName)}`,
+      index: parseInt(fileName.split(".")[0]),
+    });
   }
-  sidebar += "]";
-  fs.writeFileSync(sliderPath, sidebar, { encoding: "utf-8" });
+  sortSlider();
 }
 
 main().catch((err) => {
