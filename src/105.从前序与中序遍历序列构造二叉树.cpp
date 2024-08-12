6eadec9335d24da7b888adb61fba2a7ef02b9e4f
baicie <2055572879@qq.com>
2024-08-12 21:24:55 +0800

    812

---
+++ src/105.从前序与中序遍历序列构造二叉树.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=cpp
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ */
+
+// @lc code=start
+#include <vector>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+class Solution
+{
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
+    {
+        if (preorder.empty() || inorder.empty())
+            return nullptr;
+
+        TreeNode *root = new TreeNode(preorder[0]);
+        stack<TreeNode *> stk;
+        stk.push(root);
+
+        int inorderIndex = 0;
+        for (int i = 0; i < preorder.size(); i++)
+        {
+            TreeNode *node = stk.top();
+            if (node->val != inorder[inorderIndex])
+            {
+                node->left = new TreeNode(preorder[i]);
+                stk.push(node->left);
+            }
+            else
+            {
+                while (!stk.empty() && stk.top()->val == inorder[inorderIndex])
+                {
+                    node = stk.top();
+                    stk.pop();
+                    ++inorderIndex;
+                    /* code */
+                }
+                node->right = new TreeNode(preorder[i]);
+                stk.push(node->right);
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
