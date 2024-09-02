53189cc2c49ede67aefa29f7c01cfb45c81611a5
刘志伟 <2055572879@qq.com>
2024-09-02 14:28:07 +0800

    92

---
+++ src/108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+// struct TreeNode
+// {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    TreeNode *sortedArrayToBSTHelper(const vector<int> &nums, int left, int right)
+    {
+        if (left > right)
+            return nullptr;
+        int mid = left + (right - left + 1) / 2;
+        TreeNode *node = new TreeNode(nums[mid]);
+        node->left = sortedArrayToBSTHelper(nums, left, mid - 1);
+        node->right = sortedArrayToBSTHelper(nums, mid + 1, right);
+        return node;
+    }
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return sortedArrayToBSTHelper(nums, 0, nums.size() - 1);
+    }
+};
+// @lc code=end
