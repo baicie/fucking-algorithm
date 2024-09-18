f0ec00e003bbb4f3e362f43da7f2cc0abfcb426a
刘志伟 <2055572879@qq.com>
2024-09-18 14:27:34 +0800

    918

--- src/39.组合总和.cpp
+++ src/39.组合总和.cpp
@@ -9,9 +9,33 @@
 using namespace std;
 class Solution
 {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+
 public:
+    void backtrack(const vector<int> &candidates, int target, int start)
+    {
+        if (target == 0)
+        {
+            result.push_back(path);
+            return;
+        }
+        for (int i = start; i < candidates.size(); i++)
+        {
+            if (candidates[i] > target)
+            {
+                continue;
+            }
+            path.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], i);
+            path.pop_back();
+        }
+    }
     vector<vector<int>> combinationSum(vector<int> &candidates, int target)
     {
+        backtrack(candidates, target, 0);
+        return result;
     }
 };
 // @lc code=end
